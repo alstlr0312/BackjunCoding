2e63e3ada9d6b870ed21da522c849ba6592614a8
alstlr0312 <90487082+alstlr0312@users.noreply.github.com>
2023-10-24 13:29:25 +0900

    [Bronze V] Title: 개수 세기, Time: 116 ms, Memory: 15304 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.kt
@@ -0,0 +1,18 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val n = br.readLine().toInt()
+    val nums = br.readLine().split(" ")
+    val v = br.readLine().toInt()
+
+    val count = nums.count { it.toInt() == v }
+
+    bw.write(count.toString())
+    bw.flush()
+}
