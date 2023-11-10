986436eba2942f5196b34efb3fe18cf0bccb366e
alstlr0312 <90487082+alstlr0312@users.noreply.github.com>
2023-11-10 16:28:51 +0900

    [Silver III] Title: N과 M (3), Time: 1076 ms, Memory: 275452 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.kt
@@ -0,0 +1,30 @@
+lateinit var arr: Array<Int>
+
+var n = 0
+var m = 0
+val sb = StringBuilder()
+
+fun main() {
+    val nm = readLine()!!.split(" ")
+    m = nm[1].toInt()
+    n = nm[0].toInt()
+
+    arr = Array(n + 1) { i -> i }
+
+    dfs(1, 0, "")
+    print(sb.toString())
+}
+
+fun dfs(idx: Int, len: Int, str: String) {
+    if (len == m) {
+        sb.append(str).append("\n")
+        return
+    }
+
+    for (i in 1..n) {
+        if (len == 0)
+            dfs(i, 1, arr[i].toString())
+        else
+            dfs(i, len + 1, "$str ${arr[i]}")
+    }
+}
