b082d0754a078e68ee6a1e8c840da64bbf39cec2
alstlr0312 <90487082+alstlr0312@users.noreply.github.com>
2023-12-17 13:51:18 +0900

    [Silver III] Title: 돌 게임 3, Time: 92 ms, Memory: 12324 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.kt
@@ -0,0 +1,8 @@
+val br = System.`in`.bufferedReader()
+fun brr() = br.readLine().split(' ').map { it.toInt() }
+fun list()= br.readLine().split(' ').map { it.toInt() }.toIntArray()
+fun main() = with(System.out.bufferedWriter()) {
+    val n = br.readLine().toInt()
+    write(if (n % 7 == 0 || n % 7 == 2) "CY" else "SK")
+    close()
+}
