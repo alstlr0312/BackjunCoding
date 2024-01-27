42d7ae2dc25cc9f6d2f18c9cbf8c6317fbe5570a
alstlr0312 <90487082+alstlr0312@users.noreply.github.com>
2024-01-27 14:56:33 +0900

    [Silver II] Title: 안녕, Time: 88 ms, Memory: 12452 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.kt
+++ 백준/Silver/1535. 안녕/안녕.kt
@@ -1,29 +1,41 @@
 import java.util.*
+import javax.swing.text.html.HTML.Attribute.N
+
+
+
+
 
 val br = System.`in`.bufferedReader()
+
+fun list() = br.readLine().split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+fun input() = br.readLine().split(" ")
 fun brr() = br.readLine().split(' ').map { it.toInt() }
-fun list()= br.readLine().split(' ').map { it.toInt() }.toIntArray()
+private var n = 0
+private lateinit var arr : Array<Int>
+private lateinit var visit : Array<Boolean>
+private lateinit var minus : Array<Int>
+var result = Integer.MIN_VALUE
 fun main() = with(System.out.bufferedWriter()) {
     val n = br.readLine().toInt()
-    val k = 99
-    val dp = Array(n + 1) { IntArray(k + 1) }
-    val l = IntArray(n + 1)
-    val j = IntArray(n + 1)
+    val dp = IntArray(100)
+    val hp = IntArray(n + 1)
+    val joy = IntArray(n + 1)
+
     val st = StringTokenizer(br.readLine())
-    val st2 = StringTokenizer(br.readLine())
-    for (i in 1..n) {
-        l[i] = st.nextToken().toInt()
-        j[i] = st2.nextToken().toInt()
+    for (i in 0 until n) {
+        hp[i] = st.nextToken().toInt()
     }
-    for (i in 1..n) {
-        for (k in 1..k) {
-            if (l.get(i) <= k) {
-                dp[i][k] = Math.max(dp[i - 1][k - l.get(i)] + j.get(i), dp[i - 1][k])
-            } else {
-                dp[i][k] = dp[i - 1][k]
-            }
+
+    val st2 = StringTokenizer(br.readLine())
+    for (i in 0 until n) {
+        joy[i] = st2.nextToken().toInt()
+    }
+
+    for (i in 0 until n) {
+        for (j in 99 downTo hp[i]) {
+            dp[j] = Math.max(dp[j - hp[i]] + joy[i], dp[j])
         }
     }
-    write("${dp[n][k]}")
+    write("${dp[99]}")
     close()
 }
