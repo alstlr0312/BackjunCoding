c95474f76b0f6d7f9b5e58460a3f55365438a368
alstlr0312 <90487082+alstlr0312@users.noreply.github.com>
2024-02-06 12:27:42 +0900

    [Silver II] Title: 회의실 배정 2, Time: 92 ms, Memory: 12392 KB -BaekjoonHub

---
+++ 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.kt
@@ -0,0 +1,30 @@
+import java.util.*
+
+val br = System.`in`.bufferedReader()
+
+fun list() = br.readLine().split(" ").map { it.toInt() }.toMutableSet()
+fun input() = br.readLine().split(" ")
+fun brr() = br.readLine().split(' ').map { it.toInt() }
+
+fun main() {
+    val n = br.readLine().toInt()
+    val arr = IntArray(n+1)
+    val dp = IntArray(n+1)
+    var st: StringTokenizer
+    for (i in 0 until n) {
+        st = StringTokenizer(br.readLine())
+        val s = st.nextToken().toInt()
+        val e = st.nextToken().toInt()
+        val m = st.nextToken().toInt()
+        arr[i] = m
+    }
+    dp[0] = arr[0]
+    dp[1] = maxOf(arr[0], arr[1])
+    for (i in 2 until dp.size) {
+        dp[i] = maxOf(dp[i - 1], dp[i - 2] + arr[i])
+    }
+    println(dp[n - 1])
+}
+
+
+
