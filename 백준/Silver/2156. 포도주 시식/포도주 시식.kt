488c66fbaa1df217513f6cf0957ceb258e1f1850
alstlr0312 <90487082+alstlr0312@users.noreply.github.com>
2024-02-28 19:19:00 +0900

    [Silver I] Title: 포도주 시식, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.kt
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.kt
@@ -1,25 +1,37 @@
 import java.io.BufferedReader
+import java.io.BufferedWriter
 import java.io.InputStreamReader
-import java.util.StringTokenizer
+import java.io.OutputStreamWriter
+import java.math.BigInteger
+import java.util.*
+import kotlin.math.abs
+import kotlin.math.floor
 import kotlin.math.max
+import kotlin.math.min
 
+
+class InputHandler {
+    val br = System.`in`.bufferedReader()
+    fun list(): MutableSet<Int> = br.readLine().split(" ").map { it.toInt() }.toMutableSet()
+    fun input(): List<String> = br.readLine().split(" ")
+    fun brr(): List<Int> = br.readLine().split(' ').map { it.toInt() }
+}
 fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val n = br.readLine().toInt()
+    val handler = InputHandler()
+    val n = handler.br.readLine().toInt()
     val grape = Array(n+1){0}
     val dp = Array(n+1){0}
 
     for (i in 1..n) {
-        grape[i] = br.readLine().toInt()
+        grape[i] = handler.br.readLine().toInt()
     }
 
     for (i in dp.indices) {
         when (i) {
             0 -> dp[i] = grape[0]
-            1 -> dp[i] = grape[1]
-            2 -> dp[i] = grape[1] + grape[2]
-            else -> {
-                dp[i]=max(max(dp[i - 1], grape[i] + dp[i - 2]), grape[i] + grape[i - 1] + dp[i - 3])
+            1 -> dp[i] = grape[1]+grape[0]
+            2 -> dp[i] = max(dp[1], max(grape[0] + grape[2], grape[1] + grape[2]))
+            else -> { dp[i]=max(dp[i - 1], max(dp[i - 2] + grape[i], dp[i - 3] + grape[i - 1] + grape[i]))
             }
         }
     }
