034c95a0f6fa97581c124a3526efdb731bb7b8f3
alstlr0312 <90487082+alstlr0312@users.noreply.github.com>
2024-03-02 18:52:32 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 88 ms, Memory: 12280 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.kt
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.kt
@@ -1,11 +1,24 @@
 import java.io.BufferedReader
+import java.io.BufferedWriter
 import java.io.InputStreamReader
-import java.util.StringTokenizer
+import java.io.OutputStreamWriter
+import java.math.BigInteger
+import java.util.*
+import kotlin.math.abs
+import kotlin.math.floor
 import kotlin.math.max
+import kotlin.math.min
 
-fun main(args: Array<String>) {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val n =  br.readLine().toInt()
+
+class InputHandler {
+    val br = System.`in`.bufferedReader()
+    fun list(): MutableSet<Int> = br.readLine().split(" ").map { it.toInt() }.toMutableSet()
+    fun input(): List<String> = br.readLine().split(" ")
+    fun brr(): List<Int> = br.readLine().split(' ').map { it.toInt() }
+}
+fun main() {
+    val handler = InputHandler()
+    val n = handler.br.readLine().toInt()
 
     val dp = Array(101) { IntArray(10) }
     dp[1][0] = 0
@@ -16,6 +29,9 @@ fun main(args: Array<String>) {
     for(i in 2 until n+1) {
         for(j in 0 until 10) {
             when(j) {
+                //2자리 (dp[2][0]) 10, 2자리 (dp[2][4]) 34 54, 2자리 (dp[2][6]) 56 76, 
+                //3자리 (dp[3][5]) dp[3][5]=dp[2][4]+dp[2][6] 왜냐 3456,5456,5676,7656 4개
+                //근데 끝자리가 0인경우와 9일때는 낼려가거나 올라가는 한가지 경우만 가능하기에 올라가거나 내려가는 하나의 조항만 추가 
                 0 -> dp[i][j] = dp[i-1][1]
                 9 -> dp[i][j] = dp[i-1][8]
                 else -> dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % 1000000000
@@ -23,8 +39,7 @@ fun main(args: Array<String>) {
         }
     }
 
-    var ans = 0L
-    dp[n].forEach { ans += it }
-    print(ans%1000000000)
-
+    var result = 0L
+    dp[n].forEach { result += it }
+    print(result%1000000000)
 }
