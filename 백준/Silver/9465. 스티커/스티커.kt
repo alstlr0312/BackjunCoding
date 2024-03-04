bf0e202f7a66d72ad824e1b46695c2e2b54932d5
alstlr0312 <90487082+alstlr0312@users.noreply.github.com>
2024-03-04 20:19:26 +0900

    [Silver I] Title: 스티커, Time: 916 ms, Memory: 155388 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.kt
+++ 백준/Silver/9465. 스티커/스티커.kt
@@ -1,29 +1,44 @@
-val br = System.`in`.bufferedReader()
-fun brr() = br.readLine().split(' ').map { it.toInt() }
-fun list()= br.readLine().split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-fun main() = with(System.out.bufferedWriter()) {
-    val t = br.readLine().toInt()
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.math.BigInteger
+import java.util.*
+import kotlin.math.abs
+import kotlin.math.floor
+import kotlin.math.max
+import kotlin.math.min
+
+
+class InputHandler {
+    val br = System.`in`.bufferedReader()
+    fun list(): MutableSet<Int> = br.readLine().split(" ").map { it.toInt() }.toMutableSet()
+    fun input(): List<String> = br.readLine().split(" ")
+    fun brr(): List<Int> = br.readLine().split(' ').map { it.toInt() }
+}
+
+
+fun main()= with(System.out.bufferedWriter()) {
+    val handler = InputHandler()
+    val t = handler.br.readLine().toInt()
     for (i in 0 until t) {
-        val n = br.readLine().toInt()
-        val s = Array(2) { IntArray(n + 1) }
+        val n =  handler.br.readLine().toInt()
+        val stic = Array(2) { IntArray(n + 1) }
         val dp = Array(2) { IntArray(n + 1) }
         for (j in 0..1) {
-            val input = br.readLine().split(" ".toRegex()).dropLastWhile { it.isEmpty() }
-                .toTypedArray()
+            val sticker =  handler.br.readLine().split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
             for (k in 1..n) {
-                s[j][k] = input[k - 1].toInt()
+                stic[j][k] = sticker[k - 1].toInt()
             }
         }
 
-        dp[0][1] = s[0][1]
-        dp[1][1] = s[1][1]
+        dp[0][1] = stic[0][1]
+        dp[1][1] = stic[1][1]
         for (j in 2..n) {
-            dp[0][j] = Math.max(dp[1][j - 1], dp[1][j - 2]) + s[0][j]
-            dp[1][j] = Math.max(dp[0][j - 1], dp[0][j - 2]) + s[1][j]
+            dp[0][j] = Math.max(dp[1][j - 1], dp[1][j - 2]) + stic[0][j]
+            dp[1][j] = Math.max(dp[0][j - 1], dp[0][j - 2]) + stic[1][j]
         }
         write("${Math.max(dp[0][n], dp[1][n])}\n")
     }
     close()
 }
-
-
